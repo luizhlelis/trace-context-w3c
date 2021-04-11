20a80ab7688a733dfe08d31badfe7c9536c1a2e4
Luiz Lelis <luizhlelis@gmail.com>
2021-04-11 15:55:06 -0300

    chore: trace stdout update

--- src/system-diagnostics-activity/Program.cs
+++ src/system-diagnostics-activity/Program.cs
@@ -1,20 +1,32 @@
 ﻿using System;
 using System.Diagnostics;
 
-var parentActivity = new Activity("Parent");
+var upstreamActivity = new Activity("Upstream");
 
-parentActivity.Start();
-Console.WriteLine("{0} activity id: {1}", parentActivity.OperationName, parentActivity.Id);
+upstreamActivity.Start();
+Console.WriteLine("-------------------- {0} --------------------", upstreamActivity.OperationName);
+Console.WriteLine("traceparent: {0}", upstreamActivity.Id);
+Console.WriteLine("tracestate: {0}", upstreamActivity.TraceStateString);
+Console.WriteLine("trace-id: {0}", upstreamActivity.TraceId);
+Console.WriteLine("span-id: {0}", upstreamActivity.SpanId);
+Console.WriteLine("trace-flags: {0}", upstreamActivity.ActivityTraceFlags);
+Console.WriteLine("upstream traceparent: {0}", upstreamActivity.ParentId);
 CallChildActivity();
-parentActivity.Stop();
+upstreamActivity.Stop();
 
 Console.ReadKey();
 
 void CallChildActivity()
 {
-    var childActivity = new Activity("Child");
+    var downstreamActivity = new Activity("Downstream Dependency");
 
-    childActivity.Start();
-    Console.WriteLine("{0} activity, id: {1}", childActivity.OperationName, childActivity.Id);
-    childActivity.Stop();
+    downstreamActivity.Start();
+    Console.WriteLine("-------------------- {0} --------------------", downstreamActivity.OperationName);
+    Console.WriteLine("traceparent: {0}", downstreamActivity.Id);
+    Console.WriteLine("tracestate: {0}", downstreamActivity.TraceStateString);
+    Console.WriteLine("trace-id: {0}", downstreamActivity.TraceId);
+    Console.WriteLine("span-id: {0}", downstreamActivity.SpanId);
+    Console.WriteLine("trace-flags: {0}", downstreamActivity.ActivityTraceFlags);
+    Console.WriteLine("upstream traceparent: {0}", downstreamActivity.ParentId);
+    downstreamActivity.Stop();
 }
